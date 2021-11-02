3fb8900f438a385d024c0d41a5b19e1bcc66e353
RuiFilipeCampos <ruifilipedesousacampos@gmail.com>
2021-11-02 11:57:00 +0000

    starting an api2

---
+++ backend/topic/urls.py
@@ -0,0 +1,21 @@
+
+# Django Imports <
+from django.conf.urls import url
+
+## importing the views
+from topic import views as view
+
+
+
+urlpatterns = [
+    url(
+        "topic/",
+        view.TopicList.as_view(),
+        name="topic_list"
+        ),
+    url(
+        "topic/<str:username>/<str:topic_name>",
+        view.Topic.as_view(),
+        name="topic"
+        ),
+]
---
+++ backend/topic/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.views.generic import View
+
+
+
+
+
+
+
+
+
+
+class Topic(View):
+    def get(self, request, *args, **kwargs):
+        pass
+    
+
+
+class TopicList(View):
+    def get(self, request, *args, **kwargs):
+        pass
+    
