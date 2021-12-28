6c43f84f955b996094566fe3c3bc45d20a6cb829
Rui Campos <ruifilipedesousacampos@gmail.com>
2021-12-28 10:54:08 +0000

    Created the sqlite3 file.

--- backend/APIgateway/src/controller.py
+++ backend/APIgateway/src/controller.py
@@ -1,6 +1,6 @@
 from APIgateway.src.settings import PRIVATE_KEY, SERVER_KEY
 from errors import *
-
+import model
 
 def auth(username = "", password = ""):
     if username == "" or password == "":
@@ -9,12 +9,19 @@ def auth(username = "", password = ""):
     __password = server_encrypt(SERVER_KEY,
         ssh_decrypt(PRIVATE_KEY, password))
     
-    if get(user)["password"] == __password:
+    if model.User.get(username).password == __password:
         generate_jwt()
 
     return 200, None
 
 def new_user(username, password):
+    try:
+        model.User.get(username)
+        return 400
+
+    except UserDoesNotExist:
+        model.User.exec("")
+
     return 200
 
 
