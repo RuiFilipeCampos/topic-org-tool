839f7098e2e63fed6b928e18f48c12bb726343fb
Rui Campos <ruifilipedesousacampos@gmail.com>
2021-12-28 14:42:29 +0000

    Added STRAPI_TOKEN var.

--- backend/APIgateway/src/settings.py
+++ backend/APIgateway/src/settings.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from cryptography.fernet import Fernet
 
 MODULE_PATH = Path().resolve()/"src"
- 
+STRAPI_TOKEN = ""
 JWT_KEY = "B" # for encoding the JWTs
 SERVER_KEY  = Fernet.generate_key() # for encrypting data to the database, super private
 PRIVATE_KEY = "" # for decrypting the pass from user
