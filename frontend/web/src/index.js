b0693519942ff96d3e0e10fd4086d46a26bb8964
Rui Campos <ruifilipedesousacampos@gmail.com>
2022-01-01 12:52:53 +0000

    Set up of chakra provider.

--- frontend/web/src/index.js
+++ frontend/web/src/index.js
@@ -15,3 +15,21 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+import { extendTheme, ChakraProvider } from '@chakra-ui/react'
+
+const colors = {
+  brand: {
+    900: '#1a365d',
+    800: '#153e75',
+    700: '#2a69ac',
+  },
+}
+
+const theme = extendTheme({ colors })
+function App() {
+  return (
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
+  )
+}
